a8d3f348c3f66f3c3fda64c19baecf8714d42423
Saiful Ajom Khan <sajomkhan@gmail.com>
2024-03-09 20:51:47 +0600

    updated

--- components/shared/Bottomber.tsx
+++ components/shared/Bottomber.tsx
@@ -1,8 +1,42 @@
+"use client"
+import { sidebarLinks } from "@/constants"
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 
 const Bottombar = () => {
+  const pathname = usePathname()
+
   return (
-    <div>Topbar</div>
+    <section className="bottombar">
+      <div className="bottombar_container">
+        {sidebarLinks.map((link)=> {
+          const isActive =
+          (pathname.includes(link.route) && link.route.length > 1) ||
+          pathname === link.route;
+
+          if (link.route === "/profile") link.route = `${link.route}/${userId}`;
+
+        return (
+          <Link
+            href={link.route}
+            key={link.label}
+            className={`bottombar_link ${isActive && "bg-primary-500"}`}
+          >
+            <Image
+              src={link.imgURL}
+              alt={link.label}
+              width={24}
+              height={24}
+            />
+            <p className="text-light-1 text-subtle-medium max-sm:hidden">{link.label.split(/\s+/)[0]}</p>
+          </Link>
+        );
+        })}
+
+      </div>
+    </section>
   )
 }
 
--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -2,7 +2,14 @@
 
 const RightSidebar = () => {
   return (
-    <div>Topbar</div>
+    <section className="custim-scrollbar rightsidebar">
+      <div className="flex flex-1 flex-col">
+        <h3 className="text-heading4-medium text-light-1">Suggested Communities</h3>
+      </div>
+      <div className="flex flex-1 flex-col">
+        <h3 className="text-heading4-medium text-light-1">Suggested Users</h3>
+      </div>
+    </section>
   )
 }
 
